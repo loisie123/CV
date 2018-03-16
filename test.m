526701c7b680a4b142473b315ea0fa529f4665f6
mivadi <mmvandiepen@gmail.com>
2018-03-16 10:17:42 +0100

    trans=af

--- test.m
+++ test.m
@@ -2,9 +2,7 @@
 %% 
 [keypoints1, keypoints2, matches] = keypoint_matching_MIRTHE('boat1.pgm', 'boat2.pgm', false);
 %% 
-disp("yooy")
 RANSAC_MIRTHE( 'boat1.pgm', 'boat2.pgm');
-disp("yoo")
 %%
 disp("gvd")
 stitch('left.jpg', 'right.jpg');
